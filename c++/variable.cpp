6743700c72e253d0519d2d8f86830020c196b6a0
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-09-29 17:29:40 +0530

    Add files via upload

---
+++ c++/variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int
+    int x = 10;
+    long y = 1000000000;
+    long long z = 1000000000000000;
+    float a = 1.23;
+    double b = 10000.39303534;
+    string s = "shivkumar";
+    cout << " value of X " << x << " " << y << " " << z << " " << s << " " << a << " " << b;
+    return 0;
+}
