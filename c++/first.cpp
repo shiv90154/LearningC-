c6ad0c959cd112b747dd9d7e7973bb073f7ca9cd
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-09-29 17:13:07 +0530

    Add files via upload

---
+++ c++/first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << "valueof x" << x << "and y" << y
+         << "\n";
+    cout << "hey my name is shiv kumar i am start learning C++ ";
+    return 0;
+}
